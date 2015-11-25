cb69d7c5172b0ffc9b42c7502aceb7d40286485b
ZYHZ <552153678@qq.com>
2015-11-25 10:15:13 +0800

    Radio对应的fragment的实现

--- app/src/main/java/com/qf/designbook/MainActivity.java
+++ app/src/main/java/com/qf/designbook/MainActivity.java
@@ -1,13 +1,44 @@
 package com.qf.designbook;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
-public class MainActivity extends ActionBarActivity {
+import fragment.MapDepotFragment;
+
+public class MainActivity extends FragmentActivity implements RadioGroup.OnCheckedChangeListener{
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initview();
+    }
+
+    private void initview() {
+        radioGroup = (RadioGroup) findViewById(R.id.rg_tab);
+        radioGroup.setOnCheckedChangeListener(this);
+        radioGroup.getChildAt(0).performClick();
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId){
+            case R.id.rb_anli_main:
+
+                break;
+            case R.id.rb_tuku_main:
+                MapDepotFragment mapDepotFragment = new MapDepotFragment();
+                fragmentManager(mapDepotFragment);
+                break;
+        }
+    }
+    private void fragmentManager(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_fragment, fragment)
+                .commit();
     }
 }
---
+++ app/src/main/java/fragment/MapDepotFragment.java
@@ -0,0 +1,9 @@
+package fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2015/11/25.
+ */
+public class MapDepotFragment extends Fragment{
+}
